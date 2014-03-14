1027324ae55e0ac963481cd0b2eb6e48799f12b6
fzk <zkfan@zkfan-macbookpro2.roam.corp.google.com>
2014-03-14 23:33:53 +0800

    First pages commit

--- js/application.js
+++ js/application.js
@@ -1,4 +1,5 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var global_game = null;
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  global_game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
